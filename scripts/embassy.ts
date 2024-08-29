0a1e6e450dd9c6e75e40628c350dc9e3606e5140
Shinoa Fores <btcinfo@sdf.org>
2024-08-29 09:05:19 -0500

    Add service scripts

---
+++ scripts/embassy.ts
@@ -0,0 +1,6 @@
+// @ts-check
+export { setConfig } from "./services/setConfig.ts";
+export { properties } from "./services/properties.ts";
+export { getConfig } from "./services/getConfig.ts";
+export { action } from "./services/action.ts";
+export { migration } from "./services/migrations.ts";
